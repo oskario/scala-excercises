5a3a89e7fb139a944bcdcd6ceb91b4887d9e128c
Oskar <oskario2007@gmail.com>
2014-06-24 23:53:59 +0200

    Ex1 implementation added

--- src/main/scala/Ex1PascalsTriangle.scala
+++ src/main/scala/Ex1PascalsTriangle.scala
@@ -1,5 +1,11 @@
 object Ex1PascalsTriangle {
 
-  def pascal(row: Int, column: Int): Int = ???
-
+  def pascal(row: Int, column: Int): Int = {
+    if (row < 0 || column < 0 || column > row)
+      throw new IllegalArgumentException("Invalid position")
+    else if (column == 0 || row == 0 || column == row)
+      1
+    else
+      pascal(row - 1, column - 1) + pascal(row - 1, column)
+  }
 }
