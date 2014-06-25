c5633e71d4026285bc681dad117d49b02fbb660f
Oskar <oskario2007@gmail.com>
2014-06-25 19:33:55 +0200

    Exercise 2 tests expanded

--- src/test/scala/Ex2FibonacciSumSpec.scala
+++ src/test/scala/Ex2FibonacciSumSpec.scala
@@ -6,12 +6,18 @@ class Ex2FibonacciSumSpec extends FlatSpec with Matchers {
     Ex2FibonacciSum.fibonacciSum(0) should equal(0)
     Ex2FibonacciSum.fibonacciSum(1) should equal(1)
     Ex2FibonacciSum.fibonacciSum(3) should equal(4)
-    Ex2FibonacciSum.fibonacciSum(7) should equal(34)
+    Ex2FibonacciSum.fibonacciSum(7) should equal(33)
   }
 
   it should "report an error for invalid values" in {
     a [IllegalArgumentException] should be thrownBy {
       Ex2FibonacciSum.fibonacciSum(-1)
     }
+  }
+
+  it should "work correctly for big values" in {
+    Ex2FibonacciSum.fibonacciSum(10) should equal(143)
+    Ex2FibonacciSum.fibonacciSum(20) should equal(17710)
+    Ex2FibonacciSum.fibonacciSum(40) should equal(267914295)
   }
 }
