68281a283e42ec24b8320b673ef05188aff870d8
Oskar <oskario2007@gmail.com>
2014-06-25 19:34:21 +0200

    Exercise 2 solution added

--- src/main/scala/Ex2FibonacciSum.scala
+++ src/main/scala/Ex2FibonacciSum.scala
@@ -1,10 +1,14 @@
 object Ex2FibonacciSum {
 
+  lazy val fibonacciStream: Stream[Int] = 1 #:: 1 #:: fibonacciStream.zip(fibonacciStream.tail).map(e => e._1 + e._2)
+
   /**
    * Calculates sum of the first n fibonacci numbers.
    * @param n number of fibonacci elements to sum
    * @return Sum of first n fibonacci numbers
    */
-  def fibonacciSum(n: Int): Int = ???
-
+  def fibonacciSum(n: Int): Int = {
+    if (n < 0) throw new IllegalArgumentException("Invalid number")
+    else fibonacciStream.take(n).sum
+  }
 }
