75c5a3e3253bb0ffe907bb7bfe67bdd4a0f72b7d
Oskar <oskario2007@gmail.com>
2014-06-25 19:46:50 +0200

    Exercise 3 tests added

---
+++ src/main/scala/Ex3RPN.scala
@@ -0,0 +1,9 @@
+object Ex3RPN {
+
+  /**
+   * Evaluates given NPM-noted expression.
+   * @param expression expression to be evaluated
+   * @return The result of the expression
+   */
+  def rpn(expression: String): Double = ???
+}
---
+++ src/test/scala/Ex3RPN.scala
@@ -0,0 +1,29 @@
+import org.scalatest._
+
+class Ex3RPN extends FlatSpec with Matchers {
+  "Exercise 3" should
+    "work for simple examples" in {
+    Ex3RPN.rpn("2 2 +") should equal(4)
+    Ex3RPN.rpn("2 3 + 5 *") should equal(25)
+    Ex3RPN.rpn("5 5 + 2 /") should equal(5)
+  }
+
+  it should "report an error for invalid values" in {
+    a [IllegalArgumentException] should be thrownBy {
+      Ex3RPN.rpn("-")
+    }
+
+    a [IllegalArgumentException] should be thrownBy {
+      Ex3RPN.rpn("1 1")
+    }
+
+    a [IllegalArgumentException] should be thrownBy {
+      Ex3RPN.rpn("2 3 + *")
+    }
+  }
+
+  it should "work correctly for more complicated examples" in {
+    Ex3RPN.rpn("2 2 + 2 2 + +") should equal(4)
+    Ex3RPN.rpn(" 2 10 + 2 / 14 3 - 4 * + 2 /") should equal(11)
+  }
+}
