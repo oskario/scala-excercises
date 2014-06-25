36f2a2871fc3b6ceb8bc626172c6e32d0bb18fdc
Oskar <oskario2007@gmail.com>
2014-06-26 00:10:35 +0200

    Exercise 3 solution added

--- src/main/scala/Ex3RPN.scala
+++ src/main/scala/Ex3RPN.scala
@@ -1,3 +1,6 @@
+import java.lang.NumberFormatException
+import scala.util.Try
+
 object Ex3RPN {
 
   /**
@@ -5,5 +8,48 @@ object Ex3RPN {
    * @param expression expression to be evaluated
    * @return The result of the expression
    */
-  def rpn(expression: String): Double = ???
+  def rpn(expression: String): Double = {
+    evaluate(expression.trim.split(" "), List[Double]())
+  }
+
+  /**
+   * Evaluates given expression (recursively) using provided stack.
+   * @param input input expression
+   * @param stack stack
+   * @return Result of given expression
+   */
+  private def evaluate(input: Seq[String], stack: List[Double]): Double = {
+    if (input.size > 0) {
+      input.head match {
+        case "+" => evaluate(input.tail, eval((a, b) => b + a, stack))
+        case "-" => evaluate(input.tail, eval((a, b) => b - a, stack))
+        case "*" => evaluate(input.tail, eval((a, b) => b * a, stack))
+        case "/" => evaluate(input.tail, eval((a, b) => b / a, stack))
+        case char =>
+//          try {
+            evaluate(input.tail, char.toInt :: stack)
+//          } catch {
+//            case ex: NumberFormatException =>
+//              throw new IllegalArgumentException(s"Illegal token in expression: $char")
+//          }
+
+      }
+    } else {
+      if (stack.size == 1) stack.head
+      else throw new IllegalArgumentException("Invalid expression (unexpected end of input)")
+    }
+  }
+
+  /**
+   * Takes first two values from the stack, applies given operation
+   * and puts the result back onto the stack.
+   * @param f operation to apply
+   * @param stack stack of numbers
+   * @return New stack with result on top
+   */
+  private def eval(f: (Double, Double) => Double, stack: List[Double]): List[Double] = {
+    if (stack.size < 2) throw new IllegalArgumentException("Invalid expression")
+    val a :: b :: t = stack
+    f(a, b) :: t
+  }
 }
