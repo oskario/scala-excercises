c88fd54500b054ff6f4d04666b5a17439b1175eb
Oskar <oskario2007@gmail.com>
2014-06-26 21:13:39 +0200

    Exercise 4 solution added

--- src/main/scala/Ex4ParenthesesBalancing.scala
+++ src/main/scala/Ex4ParenthesesBalancing.scala
@@ -5,5 +5,15 @@ object Ex4ParenthesesBalancing {
    * @param sentence input sentence
    * @return True if parentheses are balanced, otherwise false
    */
-  def isBalanced(sentence: String): Boolean = ???
+  def isBalanced(sentence: String): Boolean = isBalanced(sentence, 0)
+
+  private def isBalanced(sentence: String, count: Int): Boolean = {
+    if (count < 0) false
+    else if (sentence.size == 0) count == 0
+    else sentence.head match {
+      case '(' => isBalanced(sentence.tail, count + 1)
+      case ')' => isBalanced(sentence.tail, count - 1)
+      case _ => isBalanced(sentence.tail, count)
+    }
+  }
 }
